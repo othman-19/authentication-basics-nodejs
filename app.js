826bb21b628c5ad467ff3f3d57fe5b68eb522b35
othmane <othman-19@hotmail.fr>
2020-10-20 22:11:04 +0100

    create the app.js file

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const mongoDb = "YOUR MONGO URL HERE";
+mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
+const User = mongoose.model(
+  "User",
+  new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true }
+  })
+);
+
+const app = express();
+app.set("views", __dirname);
+app.set("view engine", "ejs");
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.render("index"));
+
+app.listen(3000, () => console.log("app listening on port 3000!"));
