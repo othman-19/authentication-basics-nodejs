1db6d52e8ce14f9c3746be4f904cd4023b4c80a2
othmane <othman-19@hotmail.fr>
2020-10-26 10:51:41 +0100

    send logged in user with the '/' route

--- app.js
+++ app.js
@@ -67,7 +67,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => {
+  res.render("index", { user: req.user });
+});
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 app.post("/sign-up", (req, res, next) => {
   const user = new User({
